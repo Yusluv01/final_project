7e98e076bcda50b95cacfcdc457b2f3cce5abbbd
Yusluv <yusufayodeji2007@gmail.com>
2026-08-20 13:34:27 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,9 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
+python manage.py populate_data
 python manage.py shell -c "
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
