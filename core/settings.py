0ff413fae6472663bf9d18985c988402b6029f70
Yusluv <yusufayodeji2007@gmail.com>
2026-08-20 11:57:16 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -276,9 +276,17 @@ STATICFILES_STORAGE = (
 # MEDIA FILES
 # ============================================================
 
-MEDIA_URL = '/media/'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
 
-MEDIA_ROOT = BASE_DIR / 'media'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# MEDIA_URL = '/media/'
+
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # ============================================================
