f9c75253fe1c52df8d41e3b4d3e6fab50b213a1f
Yusluv <yusufayodeji2007@gmail.com>
2026-08-20 08:54:41 +0100

    Enhance build script to create admin user
    Added script to create a superuser if it doesn't exist.

--- build.sh
+++ build.sh
@@ -6,3 +6,28 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = '$ADMIN_USERNAME'
+email = '$ADMIN_EMAIL'
+password = '$ADMIN_PASSWORD'
+
+if not User.objects.filter(username=username).exists():
+    user = User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password
+    )
+
+    if hasattr(user, 'role'):
+        user.role = 'admin'
+        user.save(update_fields=['role'])
+
+    print('Admin account created successfully.')
+else:
+    print('Admin account already exists.')
+"
